8a0fbad534cba1f24feea870f49f4f71f148ed8f
ZhmurkoT <111373748+ZhmurkoT@users.noreply.github.com>
2022-08-18 02:47:49 +0300

    upd

--- Test.py
+++ Test.py
@@ -1,10 +1,9 @@
-number = int(input('input an edge namber '))
+number = int(input('input a namber: '))
 counter1 = 0
 counter2 = 0
 
 while counter1 < number:
     counter1 += 1
     counter2 += counter1
-
 
 print(counter2/counter1)
